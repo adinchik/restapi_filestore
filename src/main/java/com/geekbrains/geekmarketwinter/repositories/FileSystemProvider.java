c5b637090d8d2d78906f3241dff4518b4f499eac
adinchik <1chik.leoprog@gmail.com>
2022-03-13 23:36:35 +0600

    spring hw 9

--- src/main/java/com/geekbrains/geekmarketwinter/repositories/FileSystemProvider.java
+++ src/main/java/com/geekbrains/geekmarketwinter/repositories/FileSystemProvider.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
@@ -44,25 +43,38 @@ public class FileSystemProvider implements IFileSystemProvider {
     }
 
     @Override
-    public String storeFile(byte[] content, UUID md5, String fileName) throws IOException {
-        String fileNameExtension = FilenameUtils.getExtension(fileName);
-        fileName = String.format("%s.%s", md5, fileNameExtension);
-
-        Path fullFileNamePath = Paths.get(storePath.toString(), fileName);
-        String fullFileName = fullFileNamePath.toString();
-
-        if (!Files.exists(fullFileNamePath)) {
-            FileUtils.writeByteArrayToFile(new File(fullFileName), content);
-        }
-        return fullFileName;
+    public void storeFile(byte[] content, UUID md5, String fileName) throws IOException {
+        FileUtils.writeByteArrayToFile(new File(getFullFileName(md5, fileName)), content);
     }
 
 
     @Override
-    public void deleteFile(String fileHash) throws IOException {
-        Path path = Paths.get(fileHash);
+    public void deleteFile(String fileHash, String filename) throws IOException {
+        Path path = getPath(UUID.fromString(fileHash), filename);
+        System.out.println(path.toString());
         if (Files.exists(path)) {
             Files.delete(path);
         }
+    }
+
+    @Override
+    public Path getPath(UUID md5, String fileName) {
+        String fileNameExtension = FilenameUtils.getExtension(fileName);
+        fileName = String.format("%s.%s", md5, fileNameExtension);
+
+        Path fullFileNamePath = Paths.get(storePath.toString(), fileName);
+        return fullFileNamePath;
+    }
+
+    @Override
+    public String getFullFileName(UUID md5, String fileName) {
+        Path fullFileNamePath = getPath(md5, fileName);
+        return fullFileNamePath.toString();
+    }
+
+    @Override
+    public boolean isFileExist(UUID md5, String fileName){
+        Path fullFileNamePath = getPath(md5, fileName);
+        return Files.exists(fullFileNamePath);
     }
 }
